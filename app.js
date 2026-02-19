fb51d4c9b1f8ddb7b1006b00b7bfe40e88b94b26
Daryn Gregory <daryn.greg@icloud.com>
2026-02-18 23:18:19 -0500

    completed get method 2 getEmployees

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const app = express();
 export default app;
 
 app.get("/", helloEmployees);
+app.get("/employees", getEmployees);
 
 function helloEmployees(req, res) {
   res.send("Hello employees!");
+}
+
+function getEmployees(req, res) {
+  res.send(employees);
 }
