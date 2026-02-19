47fd5d114344444cc3fe2ed250894afd601d685b
Daryn Gregory <daryn.greg@icloud.com>
2026-02-18 23:19:15 -0500

    completed get method 3 getEmployeeById

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ export default app;
 
 app.get("/", helloEmployees);
 app.get("/employees", getEmployees);
+app.get("/employees/:id", getEmployeeById);
 
 function helloEmployees(req, res) {
   res.send("Hello employees!");
@@ -13,4 +14,15 @@ function helloEmployees(req, res) {
 
 function getEmployees(req, res) {
   res.send(employees);
+}
+
+function getEmployeeById(req, res) {
+  const id = Number(req.params.id);
+  const employee = employees.find((employee) => employee.id === id);
+  if (employee === undefined) {
+    return res
+      .status(404)
+      .send("That employee does not exist within our database.");
+  }
+  res.send(employee);
 }
